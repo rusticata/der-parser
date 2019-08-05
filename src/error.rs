89c5b8543c3473b85ea38c59cef45c4cec0a1c47
Pierre Chifflier <chifflier@wzdftpd.net>
2019-08-05 09:22:08 +0200

    Document error mod

--- src/error.rs
+++ src/error.rs
@@ -1,33 +1,46 @@
+//! Error type for BER/DER parsers
+
 use nom::error::{ErrorKind, ParseError};
 
+/// Error for BER/DER parsers
 #[derive(Debug, PartialEq)]
 pub enum BerError {
-    /// Ber object does not have the expected type
+    /// BER object does not have the expected type
     BerTypeError,
+    /// BER object does not have the expected value
     BerValueError,
 
     InvalidTag,
     InvalidClass,
     InvalidLength,
 
+    /// DER object was expected to be constructed (and found to be primitive)
     ConstructExpected,
+    /// DER object was expected to be primitive (and found to be constructed)
     ConstructUnexpected,
 
-    /// Ber integer is too large to fit in a native type. Use `as_bigint()`
+    /// BER integer is too large to fit in a native type. Use `as_bigint()`
     IntegerTooLarge,
 
+    /// BER recursive parsing reached maximum depth (See
+    /// [MAX_RECURSION](../ber/constant.MAX_RECURSION.html))
     BerMaxDepth,
 
     /// When parsing a defined sequence, some items could not be found
     ObjectTooShort,
 
+    /// A DER constraint failed (object may be using BER encoding?)
     DerConstraintFailed,
 
     UnknownTag,
+    /// Feature is not yet implemented
     Unsupported,
 
+    /// Custom error type left for parsers on top of this crate, so they can handle their custom
+    /// errors
     Custom(u32),
 
+    /// Error raised by the underlying nom parser
     NomError(ErrorKind),
 }
 
