006f3b2d29a47863ed48a089f75a4d042fc3ed6f
Pierre Chifflier <chifflier@wzdftpd.net>
2021-07-12 16:02:05 +0200

    Remove xor_option (MSRV is 1.44)

--- src/ber/ber.rs
+++ src/ber/ber.rs
@@ -597,16 +597,6 @@ impl<'a> From<BerObjectContent<'a>> for BerObject<'a> {
     }
 }
 
-/// Replacement function for Option.xor (>= 1.37)
-#[inline]
-pub(crate) fn xor_option<T>(opta: Option<T>, optb: Option<T>) -> Option<T> {
-    match (opta, optb) {
-        (Some(a), None) => Some(a),
-        (None, Some(b)) => Some(b),
-        _ => None,
-    }
-}
-
 /// Compare two BER headers. `len` fields are compared only if both objects have it set (same for `raw_tag`)
 impl<'a> PartialEq<BerObjectHeader<'a>> for BerObjectHeader<'a> {
     fn eq(&self, other: &BerObjectHeader) -> bool {
@@ -622,7 +612,7 @@ impl<'a> PartialEq<BerObjectHeader<'a>> for BerObjectHeader<'a> {
             }
             && {
                 // it tag is present for both, compare it
-                if xor_option(self.raw_tag, other.raw_tag).is_none() {
+                if self.raw_tag.xor(other.raw_tag).is_none() {
                     self.raw_tag == other.raw_tag
                 } else {
                     true
