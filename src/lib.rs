0b8be46e3916070bd96c8f1165df15e00593fc09
Daniel McCarney <daniel@binaryparadox.net>
2024-05-29 17:32:53 -0400

    lib: fix list items missing indentation
    Fixes a clippy finding of the form:
    
    ```
    error: doc list item missing indentation
       --> src/lib.rs:109:4
        |
    109 | //! for sequences and sets variants
        |    ^
        |
        = help: if this is supposed to be its own paragraph, add a blank line
        = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#doc_lazy_continuation
        = note: `-D clippy::doc-lazy-continuation` implied by `-D warnings`
        = help: to override `-D warnings` add `#[allow(clippy::doc_lazy_continuation)]`
    ```

--- src/lib.rs
+++ src/lib.rs
@@ -105,8 +105,11 @@
 //!
 //! The second (and preferred) parsing method is to specify the expected objects recursively. The
 //! following functions can be used:
+//!
 //! - [`parse_ber_sequence_defined`](ber/fn.parse_ber_sequence_defined.html) and similar functions
+//!
 //! for sequences and sets variants
+//!
 //! - [`parse_ber_tagged_explicit`](ber/fn.parse_ber_tagged_explicit.html) for tagged explicit
 //! - [`parse_ber_tagged_implicit`](ber/fn.parse_ber_tagged_implicit.html) for tagged implicit
 //! - [`parse_ber_container`](ber/fn.parse_ber_container.html) for generic parsing, etc.
